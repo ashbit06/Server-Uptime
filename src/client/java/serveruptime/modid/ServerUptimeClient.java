d99b1a37c94274cdde1a4d7093344d76dc5737d1
ashbit_ <ashbit0192@gmail.com>
2025-05-19 23:22:27 -0400

    Initial commit

---
+++ src/client/java/serveruptime/modid/ServerUptimeClient.java
@@ -0,0 +1,10 @@
+package serveruptime.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerUptimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
